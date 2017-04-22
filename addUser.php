7da05d6b2aecbcc00220f8266f5d446b7a190302
Shreyas Walvekar <shreyas.whis@gmail.com>
2017-04-22 13:32:56 +0530

    user add file corrected

--- addUser.php
+++ addUser.php
@@ -2,9 +2,6 @@
 		require_once __DIR__ . '/conn.php';
     	$sql='SELECT * FROM dummy WHERE email ='.$email;
 		$res= $conn->query($sql);
-		else
-		{
-			$hash = md5( rand(0,1000) );
 			//$password = rand(1000,5000);
 
 			$sql= 'INSERT INTO users(username, password, homeLat, homeLon, status, dist) 
@@ -16,7 +13,6 @@
 							 0.5)';
 			if($conn->query($sql) === TRUE)
 			{
-				
 				echo 'PASS';
 
 			}
