0079c22c7ae5cdb5782c7a8e5f524311e4c4f850
Shreyas Walvekar <shreyas.whis@gmail.com>
2017-04-23 18:47:59 +0530

    override update added and fixed

--- update.php
+++ update.php
@@ -5,29 +5,45 @@
     $hlon = 80.154147; //fetch from database
     $thr = 0.2;
 
-    if(isset($_POST['lat']) && isset($_POST['lon']))
+    if(isset($_POST['lat']) && isset($_POST['lon']) && isset($_POST['upd']))
     {
         $id=1;
         $dlat = $_POST['lat'];
         $dlon = $_POST['lon'];
-        $dis = distance($hlat, $hlon, $dlat, $dlon);
-        if ($dis < $thr)
+        $upd = $_POST['upd'];
+        if($upd === 'true')
         {
-            $sql = 'SELECT status FROM users Where ID = '.$id;
-            $res = $conn->query($sql);
-            $row = $res->fetch_assoc();
-            $status = $row['status'];
-            if ( $status == 0)
+            $dis = distance($hlat, $hlon, $dlat, $dlon);
+            if ($dis < $thr)
             {
-                $sql = 'UPDATE users SET status = 1, dist = \''.$dis.'\' WHERE id = '.$id;
+                $sql = 'SELECT status FROM users Where ID = '.$id;
+                $res = $conn->query($sql);
+                $row = $res->fetch_assoc();
+                $status = $row['status'];
+                if ( $status == 0)
+                {
+                    $sql = 'UPDATE users SET status = 1, dist = \''.$dis.'\' WHERE id = '.$id;
+                    if ($conn->query($sql) === TRUE)
+                    {
+                        echo "ON";
+                        $sql = "SELECT fkey FROM users WHERE id = ".$id;
+                        $res = $conn->query($sql);
+                        $row = $res->fetch_assoc();
+                        $regId = $row['fkey'];
+                        include 'firebase/send.php';
+                    }
+                    else
+                    {
+                        echo "Error updating record: " . $conn->error;
+                    }
+                }
+            }
+            else
+            {
+                $sql = 'UPDATE users SET status = 0, dist = \''.$dis.'\' WHERE id = '.$id;
                 if ($conn->query($sql) === TRUE)
                 {
-                    echo "ON";
-                    $sql = "SELECT fkey FROM users WHERE id = ".$id;
-                    $res = $conn->query($sql);
-                    $row = $res->fetch_assoc();
-                    $regId = $row['fkey'];
-                    include 'firebase/send.php';
+                    echo "OFF";
                 }
                 else
                 {
@@ -37,14 +53,17 @@
         }
         else
         {
-            $sql = 'UPDATE users SET status = 0, dist = \''.$dis.'\' WHERE id = '.$id;
-            if ($conn->query($sql) === TRUE)
+            $sql = 'SELECT status FROM users Where ID = '.$id;
+            $res = $conn->query($sql);
+            $row = $res->fetch_assoc();
+            $status = $row['status'];
+            if ( $status == 0)
             {
                 echo "OFF";
             }
             else
             {
-                echo "Error updating record: " . $conn->error;
+                echo "ON";
             }
         }
     }
